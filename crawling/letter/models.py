ffd6a53f53340b93827e7d0dda20b66f1656d9a9
dlcoqls <annie040601@gmail.com>
2023-08-01 14:52:51 +0900

    first commit

---
+++ crawling/letter/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class letter(models.Model) :
+    title = models.CharField(max_length=100)
+    write = models.CharField(max_length=100)
+    send = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False)
+    contents = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
